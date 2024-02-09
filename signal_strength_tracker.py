0f81ed302187fac0f5d1eb3f75f3bc73a28fa7c0
Maxwell Lou <maxlou2005@gmail.com>
2024-02-08 21:52:57 -0500

    add main logic for script (not tested)

---
+++ signal_strength_tracker.py
@@ -0,0 +1,38 @@
+"""
+Logs LTE signal strength into a file.
+"""
+import pathlib
+import subprocess
+import time
+
+
+# Parameters
+INITIAL_COMMAND = ["minicom", "-o", "-D", "/dev/ttyUSB2"]
+# For this file, just type AT+CSQ. A newline character at the end should not affect the results
+AT_COMMAND_FILE_PATH = pathlib.Path("AT_command.txt")
+# INITIAL_COMMAND = ["py"]
+# AT_COMMAND_FILE_PATH = pathlib.Path("input.txt")  # Used for testing, just has print("hello world")
+OUTPUT_LOG_FILE_PATH = pathlib.Path(f"signal_log_{int(time.time())}.txt")
+PERIOD = 2
+
+while True:
+    AT_command = open(AT_COMMAND_FILE_PATH, 'r')
+    log_file = open(OUTPUT_LOG_FILE_PATH, 'a')
+
+    result = subprocess.run(
+        INITIAL_COMMAND,
+        shell=True,
+        capture_output=True,
+        encoding='utf_8',
+        stdin=AT_command
+    )
+
+    # String parsing can be done here if you want to edit the log file output
+    log_file.write(result.stdout)
+
+    AT_command.close()
+    log_file.close()
+
+    print("logged...")
+
+    time.sleep(PERIOD)
