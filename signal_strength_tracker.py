e67a38901b82014c8ea2c971a172026a104916f6
Maxwell Lou <maxlou2005@gmail.com>
2024-02-19 11:08:55 -0500

    Edit documentation and small changes

--- signal_strength_tracker.py
+++ signal_strength_tracker.py
@@ -14,33 +14,35 @@ BAUDRATE = 115200  # Change this to match your modem's baud rate
 AT_COMMAND = b"AT+CSQ\r\n"  # Command to run on modem
 OUTPUT_LOG_FILE_PATH = pathlib.Path(f"/media/warg/E3F9-7EE3/signal_log_{int(time.time())}.txt")
 PERIOD = 3
+SETUP_TIME = 20
 
-# Wait 20 seconds becasue the RPi has a setting where it doesn't boot until 20 seconds after power
-time.sleep(20)
+if __name__ == "__main__":
+    # Wait 20 seconds becasue the RPi has a setting where it doesn't boot until 20 seconds after power
+    time.sleep(SETUP_TIME)
 
-while True:
-    log_file = open(OUTPUT_LOG_FILE_PATH, 'a')
+    while True:
+        log_file = open(OUTPUT_LOG_FILE_PATH, 'a')
 
-    # Create serial connection
-    ser = serial.Serial(
-        SERIAL_PORT,
-        baudrate=BAUDRATE,
-        timeout=1,
-    )
-    
-    # Write AT command to the modem
-    ser.write(AT_COMMAND)
+        # Create serial connection
+        ser = serial.Serial(
+            SERIAL_PORT,
+            baudrate=BAUDRATE,
+            timeout=1,
+        )
+        
+        # Write AT command to the modem
+        ser.write(AT_COMMAND)
 
-    # Get result back from the modem (reads 1000 bytes)
-    response = ser.read(1000)
+        # Get result back from the modem (reads 1000 bytes)
+        response = ser.read(1000)
 
-    # String parsing can be done here if you want to edit the log file output
-    log_file.write(
-        datetime.datetime.now().isoformat(timespec="seconds") + \
-        "    Response: " + response.decode('utf-8') + "\n"
-    )
+        # String parsing can be done here if you want to edit the log file output
+        log_file.write(
+            datetime.datetime.now().isoformat(timespec="seconds") + \
+            "    Response: " + response.decode('utf-8') + "\n"
+        )
 
-    log_file.close()
-    ser.close()
+        log_file.close()
+        ser.close()
 
-    time.sleep(PERIOD)
+        time.sleep(PERIOD)
