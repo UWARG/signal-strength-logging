8cd7c96d672dec7b01cc004a5f13a6e3e494b2e9
Maxwell Lou <maxlou2005@gmail.com>
2024-02-19 22:34:11 -0500

    Improve documentation

--- signal_strength_tracker.py
+++ signal_strength_tracker.py
@@ -8,38 +8,46 @@ import time
 import serial
 
 
-# Parameters
-SERIAL_PORT = "/dev/ttyUSB3"  # Change this to match your modem's serial port
-BAUDRATE = 115200  # Change this to match your modem's baud rate
-AT_COMMAND = b"AT+CSQ\r\n"  # Command to run on modem
-OUTPUT_LOG_FILE_PATH = pathlib.Path(f"/media/warg/E3F9-7EE3/signal_log_{int(time.time())}.txt")
-PERIOD = 3
-SETUP_TIME = 20
+SERIAL_PORT = "/dev/ttyUSB3"  # Change this to match the LTE modem's serial port
+BAUDRATE = 115200  # Change this to match the LTE modem's baud rate
+AT_COMMAND = b"AT+CSQ\r\n"  # Command to run on modem (returns signal strength)
+OUTPUT_LOG_FILE_PATH = pathlib.Path(
+    "/",
+    "media",
+    "warg",
+    "E3F9-7EE3",
+    f"signal_log_{int(time.time())}.txt"
+)
+TIMEOUT = 1  # Seconds
+BYTES_TO_READ = 1000  # Maximum bytes to read after command is run
+
+SETUP_TIME = 20  # Wait 20s since the RPi has a setting where it doesn't boot until 20s after power
+PERIOD = 3  # Seconds
+
 
 if __name__ == "__main__":
-    # Wait 20 seconds becasue the RPi has a setting where it doesn't boot until 20 seconds after power
     time.sleep(SETUP_TIME)
 
     while True:
-        log_file = open(OUTPUT_LOG_FILE_PATH, 'a')
+        log_file = open(OUTPUT_LOG_FILE_PATH, 'a', encoding="utf-8")
 
         # Create serial connection
         ser = serial.Serial(
             SERIAL_PORT,
             baudrate=BAUDRATE,
-            timeout=1,
+            timeout=TIMEOUT,
         )
         
         # Write AT command to the modem
         ser.write(AT_COMMAND)
 
         # Get result back from the modem (reads 1000 bytes)
-        response = ser.read(1000)
+        response = ser.read(BYTES_TO_READ)
 
         # String parsing can be done here if you want to edit the log file output
         log_file.write(
             datetime.datetime.now().isoformat(timespec="seconds") + \
-            "    Response: " + response.decode('utf-8') + "\n"
+            "    Response: " + response.decode("utf-8") + "\n"
         )
 
         log_file.close()
